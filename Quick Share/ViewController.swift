17e4978fa349eaab9908d4441d23cda8b2a36422
Alexey Yarov <alexxd91@gmail.com>
2021-06-18 13:08:35 +0300

    Initial Commit

---
+++ Quick Share/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quick Share
+//
+//  Created by Alexey Yarov on 18.06.2021.
+//  Copyright © 2021 Alexey Yarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
